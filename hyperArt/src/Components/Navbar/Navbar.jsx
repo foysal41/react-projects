c8fe97732800cf3e06ad36df0eb2fb89c856ee70
foysal41 <mdfoysal.mf33@gmail.com>
2025-12-07 21:02:26 +0600

    react project installed

---
+++ hyperArt/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div className='flex items-center gap-2'>
+            <div className='bg-indigo-500 h-8 w-8 flex items-center justify-center font-bold rounded-xl text-white'>FJ</div>
+            <span className='font-semibold text-lg uppercase'>Foysal Jaman</span>
+
+
+
+
+        </div>
+
+
+
+    </header>
+  )
+}
+
+export default Navbar
