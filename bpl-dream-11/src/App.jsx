eb25ad00546b468bef7ffcd715a23c747e04ecc8
foysal41 <mdfoysal.mf33@gmail.com>
2026-03-25 10:26:34 +0600

    install reacta and install others like tailwind, daisy ui

---
+++ bpl-dream-11/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-red-500">hello</h3>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  );
+}
+
+export default App;
