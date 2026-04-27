8f8eed5ae6c7d5f1d542bed43f4fae0de65f3953
foysal41 <mdfoysal.mf33@gmail.com>
2026-04-27 22:49:14 +0600

    Design Login & Register Page With React Hook Form

--- dragon-news-next/src/app/(auth)/login/page.jsx
+++ dragon-news-next/src/app/(auth)/login/page.jsx
@@ -1,5 +1,70 @@
+"use client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 const LoginPage = () => {
-  return <div>Login page</div>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegisterFunc = (data) => {
+    const { email, name, photo, password } = data;
+  };
+  return (
+    <div className="container mx-auto min-h-[80vh] flex justify-center bg-slate-400 ">
+      <div className="p-4 rounded-full bg-white ">
+        <h2 className="font-bold text-3xl text-center mb-3">
+          Login your account
+        </h2>
+
+        <form
+          action=""
+          className="space-y-4 "
+          onSubmit={handleSubmit(handleRegisterFunc)}
+        >
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              {...register("email")}
+              type="email"
+              className="input"
+              placeholder="Type here"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              {...register("password", {
+                required: "Password field is required",
+              })}
+              type="password"
+              className="input"
+              placeholder="Type here Password"
+            />
+            <p>
+              {errors.password && (
+                <span className="text-red-500">{errors.password.message}</span>
+              )}
+            </p>
+          </fieldset>
+
+          <button className="btn w-full bg-slate-800 text-black">
+            Register
+          </button>
+        </form>
+
+        <p className="mt-4">
+          Dont Have an Account{" "}
+          <Link href={"/register"} className="text-blue-500">
+            Register Account
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- dragon-news-next/src/app/(auth)/register/page.jsx
+++ dragon-news-next/src/app/(auth)/register/page.jsx
@@ -1,5 +1,82 @@
+"use client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 const RegisterPage = () => {
-  return <div>Register page</div>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const handleLoginFunc = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="container mx-auto min-h-[80vh] flex justify-center bg-slate-400 ">
+      <div className="p-4 rounded-full bg-white ">
+        <h2 className="font-bold text-3xl text-center mb-3">
+          Login your account
+        </h2>
+
+        <form
+          action=""
+          className="space-y-4 "
+          onSubmit={handleSubmit(handleLoginFunc)}
+        >
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Name</legend>
+            <input
+              {...register("name")}
+              type="text"
+              className="input"
+              placeholder="Type Name here"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Photo URL</legend>
+            <input
+              {...register("photo")}
+              type="text"
+              className="input"
+              placeholder="photo lnk here"
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              {...register("email")}
+              type="email"
+              className="input"
+              placeholder="Type here"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              {...register("password", {
+                required: "Password field is required",
+              })}
+              type="password"
+              className="input"
+              placeholder="Type here Password"
+            />
+            <p>
+              {errors.password && (
+                <span className="text-red-500">{errors.password.message}</span>
+              )}
+            </p>
+          </fieldset>
+
+          <button className="btn w-full bg-slate-800 text-black">Login</button>
+        </form>
+
+     
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
